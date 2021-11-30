1e4c835623847413dc036476b899aceaab687650
Omar-jerbi <Omarjerbi98@gmail.com>
2021-11-30 22:18:13 +0100

    header scheletro 60%

--- _index.php
+++ _index.php
@@ -3,8 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>PayPerWear -Index</title>
+    <link rel="stylesheet" href="css/general.css">
 </head>
 <body>
-    
+    <?php
+        include("common/header.php")
+    ?>
+
+
+
+
+
+
+    <?php 
+        include("common/footer.php")
+    ?>
 </body>
 </html>
