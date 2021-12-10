3594bc194e6526441e0da8a3a34d5bb123a958b5
Omar-jerbi <Omarjerbi98@gmail.com>
2021-12-10 18:30:10 +0100

    carrello delete ok

--- carrello.php
+++ carrello.php
@@ -102,31 +102,46 @@ let m = document.getElementById("maglia");
 let p = document.getElementById("pantalone");
 let s = document.getElementById("scarpa");
 
-if(m!=null){
-    let bm = document.createElement("input");
-    bm.id='bm';
-    bm.type = "button";
-    bm.value = "ELIMINA";
-    
-    m.append(bm);
-}
-if(p!=null){
-    let bp = document.createElement("input");
-    bp.is='bp';
-    bp.type = "button";
-    bp.value = "ELIMINA";
-    
-    p.append(bp);
-}
-if(s!=null){
-    let bs = document.createElement("input");
-    bs.type = "button";
-    bs.value = "ELIMINA";
-    
-    s.append(bs);
-}
+addButton(m);
+addButton(p);
+addButton(s);
 
-bm.addEventListener("click", () => alert("bella"));
+function addButton(elem){
+    if(elem != null){
+        let b = document.createElement("input");
+        b.id = 'b'+elem.id;
+        b.type = 'button';
+        b.value = 'Elimina Articolo';
 
+        elem.append(b);
+        
+        b.addEventListener("click", () =>{
+            switch (elem.id) {
+                case 'maglia':
+                    document.cookie = "misuramaglia=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "magliascelta=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    
+                    break;
+                case 'pantalone':
+                    document.cookie = "misurapantaloni=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "pantalonescelto=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    break;
+                case 'scarpa':
+                    document.cookie = "misurascarpe=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    document.cookie = "scarpascelta=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                    break;
+                }
+
+                let x = document.cookie.split('=');
+
+                if(x[2] == 'pieno'){
+                    document.cookie = "carrello=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+                }
+
+
+            location.reload();
+        });
+    }
+}
 </script>
 
