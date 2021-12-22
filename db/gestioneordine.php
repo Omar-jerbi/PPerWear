f131afcff5e1adf938a281d5e034fd5cdce3ef92
Omar-jerbi <Omarjerbi98@gmail.com>
2021-12-22 14:53:14 +0100

    storico ok gestione transazione OK*2
    finisci popolamento db articoli

--- db/gestioneordine.php
+++ db/gestioneordine.php
@@ -8,34 +8,30 @@
     include("connection.php");
     
     
-    $canary = 0;
-    mysqli_begin_transaction($connection);
-    try {
+    $canary = 0;//se una insert fallisce = 1 (articolo non è presente su db)
+    mysqli_begin_transaction($connection);/***************/
+     
         $stmt = mysqli_prepare($connection, "INSERT INTO `ordini` (`idordine`, `articolo`, `taglia`) VALUES (NULL, ?, ?)");
         mysqli_stmt_bind_param($stmt, 'si', $_COOKIE["magliascelta"], $_COOKIE["misuramaglia"]);
         
         if(!mysqli_stmt_execute($stmt)){
             $canary = 1;
+        }else{
+            $idordine = mysqli_insert_id($connection);//ultimo id auto_increment generato
+    
+            $stmt = mysqli_prepare($connection, "INSERT INTO `ordini` (`idordine`, `articolo`, `taglia`) VALUES (?, ?, ?)");
+            mysqli_stmt_bind_param($stmt, 'isi', $idordine, $_COOKIE["pantalonescelto"], $_COOKIE["misurapantaloni"]);
+            if(!mysqli_stmt_execute($stmt)){
+                $canary = 1;
+            }else{
+                mysqli_stmt_bind_param($stmt, 'isi', $idordine, $_COOKIE["scarpascelta"], $_COOKIE["misurascarpe"]);
+                if(!mysqli_stmt_execute($stmt)){
+                    $canary = 1;
+                }
+            }
+    
         }
-
-        $idordine = mysqli_insert_id($connection);//ultimo id auto_increment generato
-
-        $stmt = mysqli_prepare($connection, "INSERT INTO `ordini` (`idordine`, `articolo`, `taglia`) VALUES (?, ?, ?)");
-        mysqli_stmt_bind_param($stmt, 'isi', $idordine, $_COOKIE["pantalonescelto"], $_COOKIE["misurapantaloni"]);
-        if(!mysqli_stmt_execute($stmt)){
-            $canary = 1;
-        }
-
-        mysqli_stmt_bind_param($stmt, 'isi', $idordine, $_COOKIE["scarpascelta"], $_COOKIE["misurascarpe"]);
-        if(!mysqli_stmt_execute($stmt)){
-            $canary = 1;
-        }
-   
-    } catch (mysqli_sql_exception $exception)  {
-        mysqli_rollback($connection);
-    }
-
-
+        
     if($canary == 0){
         echo "<h1>CONGRATULAZIONI! In pochi giorni riceverai il tuo Wear presso l'indirizzo che ci hai fornito</h1>";
         $idutente = $_SESSION["idutente"];
@@ -43,12 +39,16 @@
         $stmt = mysqli_prepare($connection, "INSERT INTO `ordiniutenti` (`idutente`, `idordine`) VALUES (?, ?)");
         mysqli_stmt_bind_param($stmt, 'ii', $idutente, $idordine);
         mysqli_stmt_execute($stmt);
+        
+        
+        mysqli_commit($connection);/***************/
     }else{
         echo "<h1>Ci dispiace! qualcosa è andato storto...</h1>";
+        
+        mysqli_rollback($connection);/***************/
     }
-    
 
-    
+
     //annullamento cookie carrello
     setcookie("carrello", "", time() - 3600, '/');
     setcookie("magliascelta", "", time() - 3600, '/');
--- db/storico.php
+++ db/storico.php
@@ -43,51 +43,47 @@
                 include("connection.php");
                 $idutente = $_SESSION["idutente"];
 
-                $stmt = mysqli_prepare($connection, "SELECT `idordine`, `data` FROM `ordiniutenti` WHERE `idutente` = ?");
+                $stmt = mysqli_prepare($connection, "SELECT `idordine`, `data`, `articolo`, `taglia` FROM `ordiniutenti` NATURAL JOIN `ordini` WHERE `idutente` = ?");
                 mysqli_stmt_bind_param($stmt, 'i', $idutente);
                 mysqli_stmt_execute($stmt);
                 $res = mysqli_stmt_get_result($stmt);
                 
+                $prevIdordine="";
                 $i = 0;
+                
                 while(1){
-                    $arrordinidate = mysqli_fetch_array($res);
-                    if(!$arrordinidate) {
+                    $arr = mysqli_fetch_array($res);    
+                    if(!$arr){
                         if($i == 0){
                             echo "<h1>Non hai ancora fatto nessun ordine</h1>";
                         }
                         break;
                     }
                     $i++;
-
-                    $stmt2 = mysqli_prepare($connection, "SELECT `articoli`, `taglie` FROM `ordini` WHERE `idordine` = ?");
-                    mysqli_stmt_bind_param($stmt2, 'i', $arrordinidate[0]);
-                    mysqli_stmt_execute($stmt2);
-                    $resart = mysqli_stmt_get_result($stmt2);
                     
-                    $articoli = mysqli_fetch_array($resart);
+                    if($arr[0] != $prevIdordine){//idordine e data -> stamparli una volta per ordine
+                        $prevIdordine = $arr[0];
 
-                    $arrayarticoli = explode('|', $articoli[0]);
-                    $arraytaglie = explode('|', $articoli[1]);
+                        if($i != 0) echo '</li>';//nuovo ordine (chiudo li precedente)(non primo ordine)
 
-                    echo '
-                    <li>
-                        <h1>DATA ORDINE: '.$arrordinidate[1].'</h1>
-                        <img src=../'.$arrayarticoli[0].' alt="a1">
-                        <img src=../'.$arrayarticoli[1].' alt="a2">
-                        <img src=../'.$arrayarticoli[2].' alt="a3">
-                        <h3>'.
-                            ($arraytaglie[0] == "1" ?  "S" : ($arraytaglie[0] == "2" ?  "M" : ($arraytaglie[0] == "3" ?  "L" :  "XL")))
-                        .'</h3>
-                        <h3>'.$arraytaglie[1].'</h3>
-                        <h3>'.$arraytaglie[2].'</h3>
-                    </li>'
-                    ;
+                        echo '
+                        <li>
+                            <h1>ID ORDINE: '.$arr[0].'</h1>
+                            <h1>DATA ORDINE: '.$arr[1].'</h1>
+
+                            <img src=../'.$arr[2].' alt="a1">
+                            <h3>'.($arr[3] == "1" ? "S" : ($arr[3] == "2" ? "M" : ($arr[3] == "3" ? "L" : ($arr[3] == "4" ? "XL" : $arr[3])))).'</h3>
+                        ';
+
+                    }else{
+                        echo '
+                            <img src=../'.$arr[2].' alt="a1">
+                            <h3>'.($arr[3] == "1" ? "S" : ($arr[3] == "2" ? "M" : ($arr[3] == "3" ? "L" : ($arr[3] == "4" ? "XL" : $arr[3])))).'</h3>
+                        ';
+                    }
                 }
-
             ?>
         </ul>
-
-
 
     </div>
 
